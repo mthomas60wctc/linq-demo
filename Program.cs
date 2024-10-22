911eb074149f146d7eb7a9881cd6aaa185cf79a6
Mitchell Thomas <mthomas60@my.wctc.edu>
2024-10-22 13:52:14 -0500

    [1.19b] List the character(s) created in that 1981 (all series) - return character name and series only.

--- Program.cs
+++ Program.cs
@@ -114,6 +114,7 @@ Console.WriteLine("\x1b[3J");
 // [1.19a] How many character(s) were created in 1981 (all series)?
 Console.WriteLine(characters.Count(c => c.YearCreated == 1981));
 // [1.19b] List the character(s) created in that 1981 (all series) - return character name and series only.
+foreach (var obj in characters.Where(c => c.YearCreated == 1981).Select(c => new {c.Name, c.Series})) Console.WriteLine($"{obj.Name} - {String.Join(", ", obj.Series.ToArray())}");
 // [1.19c] How many character(s) were created in 1981 (Mario series)?
 // [1.19d] List the character(s) created in that 1981 (Mario series) - return character name only.
 // [1.19e] How many character(s) were created in 1981 (Donkey Kong series)?
