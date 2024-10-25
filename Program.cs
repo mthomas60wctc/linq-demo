e301656313de4cc169a02e60ae6c102c4aaf463e
Mitchell Thomas <mthomas60@my.wctc.edu>
2024-10-24 19:24:55 -0500

    List the character(s) in the Mario series that are something other than Human or Koopa species - return character name and species only.

--- Program.cs
+++ Program.cs
@@ -168,4 +168,5 @@ Console.WriteLine("\x1b[3J");
 // [1.25e] How many character(s) in the Mario series are something other than Human or Koopa species?
 Console.WriteLine(characters.Count(c => !(c.Species == "Koopa" || c.Species == "Human") && c.Series.Contains("Mario")));
 // [1.25f] List the character(s) in the Mario series that are something other than Human or Koopa species - return character name and species only.
+foreach (var obj in characters.Where(c => !(c.Species == "Koopa" || c.Species == "Human") && c.Series.Contains("Mario")).Select(c => new {c.Name, c.Species})) Console.WriteLine($"{obj.Name} - {obj.Species}");
 // [1.26] List the character(s) in the Donkey Kong series that are something other than Human or Kong species - return character name and species only?
